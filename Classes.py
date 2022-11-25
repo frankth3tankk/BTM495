50f007620faf5c04517121bb5b959ab8b52b62cd
frankth3tankk <franciscote.snow26@gmail.com>
2022-11-25 15:38:21 -0500

    Update Classes.py

--- Classes.py
+++ Classes.py
@@ -14,75 +14,176 @@ class Student:
 """
 
 class Employee:
-    def __init__(self, emp_id, emp_f_name, emp_l_name, emp_phone_num, emp_position):
+    def __init__(self, emp_id, emp_position, project, proposal, quote, contract):
         self.emp_id = emp_id
-        self.emp_f_name = emp_f_name
-        self.emp_l_name = emp_l_name
-        self.emp_phone_num = emp_phone_num
         self.emp_position = emp_position
+        project(Project)
+        proposal(Proposal)
+        quote[Quote]
+        contract[Contract]
+      
+    #Methods  
+    def sign_contract(employee_signature):
+        Contract.append(employee_signature)
         
-    def enrol(self, sign_contract):
-        sign_contract(Contract)
-        
-    
 class Client:
-    def __init__(self, id, phone_number, f_name, l_name, address, type):
+    def __init__(self, id, address, type, project, inquiry, quote, contract):
         self.id = id
-        self.f_name = f_name
-        self.l_name = l_name
-        self.phone_number = phone_number
         self.address = address
         self.type = type
+        project(Project)
+        inquiry[Inquiry]
+        quote[Quote]
+        contract[Contract]
               
-    def enrol(self, sign_contract, approve_quote, pay_invoice, generate_inquiry):
-        sign_contract(Contract)
-        approve_quote(Quote)
-        pay_invoice(Invoice)
-        generate_inquiry(Inquiry)
-        
-        
+    #Methods
+    def sign_contract(self):
+       self 
+       
+    def generate_inquiry(self):
+       self
+            
+class Person:
+    def __init__(self, firstname, lastname, phone_number, email):
+        self.firstname = firstname
+        self.lastname = lastname
+        self.phone_number = phone_number
+        self.email = email 
+              
 class Contract:
-    def __init__(self, contract_ID, release_state):
-        self.contract_ID = contract_ID
+    def __init__(self, id, description, release_state, employee_signature, client_signature, project, quote):
+        self.id = id
+        self.description = description
         self.release_state = release_state
-        
-    def enrol(self, employee_signature, client_signature):
         employee_signature(Employee)
         client_signature(Client)
+        project(Project)
+        quote(Quote)
+        
+    #Methods
+    def send_contract(self):
+        self
+        
+    def add_stage_date(self):
+       self 
+       
+    def add_quote(self):
+       self
 
 class Quote:
-    def __init__(self, quote_ID, issue_date, cost):
-        self.quote_ID = quote_ID
+    def __init__(self, id, issue_date, cost, project, employee, send_quote, add_project):
+        self.id = id
         self.issue_date = issue_date
         self.cost = cost
+        project(Project)
+        employee(Employee)
         
-class Invoice:
-    def __init__(self, invoice_ID, amount, invoice_type, date_due):
-        self.invoice_ID = invoice_ID
-        self.amount = amount
-        self.invoice_type = invoice_type
-        self.date_due = date_due
+    #Methods
+    def send_quote(self):
+       self 
+       
+    def add_project(self):
+       self
         
-    def enrol(self, charge_full_price, charge_repairs, charge_project):
-        charge_full_price(Furniture)
-        charge_repairs(Furniture)
-        charge_project(Project)
-
+class Project:       
+    def __init__(self, id, design_details, design_sketch, description, status, feasibility, progress, requirements, stage_dates, items, client, proposal, quote, contract, furniture):
+        self.id = id
+        self.design_details = design_details
+        self.design_sketch = design_sketch
+        self.description = description
+        self.status = status
+        self.feasibility = feasibility
+        self.progress = progress
+        self.requirements = requirements
+        self.stage_dates = [stage_dates]
+        items[Furniture]
+        client(Client)
+        proposal(Proposal)
+        quote(Quote)
+        contract(Contract)
+        furniture(Contract)
+        
+    #Methods
+    def change_furniture_reserved(self):
+       self
+       
+    def add_quote(self):
+       self
+       
+    def add_contract(self):
+       self
+       
+    def add_furniture(self):
+       self 
          
-class Inquiry:
-     def __init__(self):
-        self
-     
-class Proposal:       
-    def __init__(self):
-        self
-     
-class Project:
-    def __init__(self):
+class Proposal(Project):
+    def __init__(self, id, description, floor, number_of_rooms_to_fill, residence_type, dates_available_for_visit, client, inquiry):
+        self.id = id
+        self.description = description
+        self.floor = floor
+        self.number_of_rooms_to_fill = number_of_rooms_to_fill
+        self.residence_type = residence_type
+        self.dates_available_for_visit = dates_available_for_visit
+        client(Client)
+        inquiry(Inquiry)
+    
+    #Methods    
+    def modify_client_info(self):
+        self   
+                          
+class Inquiry(Project):
+    def __init__(self, id, date, description, street_name, house_number, city, province, client):
+        self.id = id
+        self.date = date
+        self.description = description
+        self.street_name = street_name
+        self.house_number = house_number
+        self.city = city
+        self.province = province
+        client(Client)
+  
+    #Methods
+    def create_profile(self):
         self
      
 class Furniture:    
-    def __init__(self):
-        self
+    def __init__(self, id, reserved_date, name, picture, address, project):
+        self.id = id
+        self.reserved_date = [reserved_date]
+        self.name = name
+        self.picture = picture
+        self.address = address
+        project(Project)
          
-        
+    
+    
+    
+ 
+'''
+if client.exist()
+    fetch client_id()
+else
+    create_profile.new(Client) and create.client_id() and fetch client_id()
+    
+with open("furniture.json", "w") as write_file:
+    json.dump(furniture, write_file) 
+    
+client = {
+    "furniture1": [{"furniture_reserved", "date" }],
+    "furniture2": [{"furniture_reserved", "date" }],
+    "furniture3": [{"furniture_reserved", "date" }] 
+} 
+
+assign date to furniture() 
+
+available = True
+
+if Furniture.furniture_reserved = Project.stage_dates
+    append().furniture_reserved as available = false
+else if
+    append().furniture_reserved as available
+ 
+if available = true   
+    append() project_items[Furniture]
+
+'''
